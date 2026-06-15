fe339f181d55b1692763daae0d6b8c0b01e126c2
antarpreetsinghbajwa <antar@palladiumsecurity.com>
2026-06-15 21:21:36 +0000

    Fixing Azure VPN SKU to VpnGw1AZ

--- azure-infrastructure/azure-main.tf
+++ azure-infrastructure/azure-main.tf
@@ -154,7 +154,7 @@ resource "azurerm_virtual_network_gateway" "vpn_gw" {
 
   active_active = false
   enable_bgp    = false
-  sku           = "VpnGw1" # Standard Generation 1 Gateway
+  sku           = "VpnGw1AZ" # Upgraded to compliance-required AZ SKU
 
   ip_configuration {
     name                          = "vnetGatewayConfig"
